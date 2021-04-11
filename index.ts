26871e3112cecb92c2fe32a996a2f6679bf94ab8
Anwesh43 <anweshthecool0@gmail.com>
2021-04-11 21:33:40 +0530

    adding constants

---
+++ index.ts
@@ -0,0 +1,16 @@
+const w : number = window.innerWidth 
+const h : number = window.innerHeight
+const lines : number = 4 
+const parts : number = 2 + lines  
+const scGap : number = 0.02 / parts 
+const strokeFactor : number = 90 
+const sizeFactor : number = 3.9 
+const delay : number = 20 
+const backColor : string = "#bdbdbd"
+const colors : Array<string> = [
+    "#f44336",
+    "#9C27B0",
+    "#2196F3",
+    "#E65100",
+    "#00C853"
+]
