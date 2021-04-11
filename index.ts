229cc8996e1c19b693290bd8b4e9e8667db31d00
Anwesh43 <anweshthecool0@gmail.com>
2021-04-11 22:22:37 +0530

    adding SPPNode class

--- index.ts
+++ index.ts
@@ -159,3 +159,45 @@ class Animator {
         }
     }
 }
+
+class SPPNode {
+
+    state : State = new State()
+    prev : SPPNode 
+    next : SPPNode 
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new SPPNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawSPPNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : SPPNode {
+        var curr : SPPNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
