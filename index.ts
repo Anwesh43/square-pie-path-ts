a1ad549a852606dba38881af4b7ccea41b59abfc
Anwesh43 <anweshthecool0@gmail.com>
2021-04-11 22:26:55 +0530

    adding SquarePiePath class

--- index.ts
+++ index.ts
@@ -201,3 +201,26 @@ class SPPNode {
         return this 
     }
 }
+
+class SquarePiePath {
+
+    curr : SPPNode = new SPPNode(0)
+    dir : number = 1 
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
